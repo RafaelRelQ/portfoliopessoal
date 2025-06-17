6acc8cd2d500f46684f1a5614a3425e799312d34
Rafael Cirino <rafael.rel.q@gmail.com>
2025-06-17 15:21:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,8 +5,32 @@ function scrollSec(id){
 
 function abrirpop1(){
     document.getElementById('popup1').classList.add("aberto");
-}
+};
 
 function fecharpop1(){
     document.getElementById('popup1').classList.remove("aberto");
-}
+};
+
+function abrirpop2(){
+    document.getElementById('popup2').classList.add("aberto");
+};
+
+function fecharpop2(){
+    document.getElementById('popup2').classList.remove("aberto");
+};
+
+function abrirpop3(){
+    document.getElementById('popup3').classList.add("aberto");
+};
+
+function fecharpop3(){
+    document.getElementById('popup3').classList.remove("aberto");
+};
+
+function abrirpop4(){
+    document.getElementById('popup4').classList.add("aberto");
+};
+
+function fecharpop4(){
+    document.getElementById('popup4').classList.remove("aberto");
+};
